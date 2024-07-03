6446642911f0f3b7ef5870c09e1711ce3c8adade
hypulse <sjinsilval28@naver.com>
2024-07-03 15:03:16 +0900

    -> v1

--- env.js
+++
@@ -1,4 +0,0 @@
-export const PB_URL = new URL(window.location.href).searchParams.get("pb_url");
-export const CONFIG_RECORD_ID = new URL(window.location.href).searchParams.get(
-  "config_record_id"
-);
--- utils/stringToHslColor.js
+++
@@ -1,6 +0,0 @@
-export default function stringToHslColor(string) {
-  const hue =
-    string.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0) % 360;
-
-  return `hsl(${hue}, 50%, 50%)`;
-}
