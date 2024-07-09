6b53b2a5aa5860b71c5f84a87a9a20f42b8705bc
hypulse <sjinsilval28@naver.com>
2024-07-10 00:06:43 +0900

    feat: Add options parameter to usePbData hook

--- v2/hooks/usePbData.js
+++ v2/hooks/usePbData.js
@@ -2,25 +2,24 @@ import { DB_PREFIX } from "../env.js";
 import { useEffect, useState } from "../libs/preact.js";
 import usePb from "./usePb.js";
 
-export default function usePbData(collectionId, recordId) {
+export default function usePbData(collectionId, recordId, options = {}) {
   collectionId = `${DB_PREFIX}_${collectionId}`;
   const { pb, authenticated } = usePb();
   const isGetOne = !!recordId;
   const defaultData = !!recordId ? {} : [];
   const [data, setData] = useState(defaultData);
   const [ready, setReady] = useState(false);
+  options.requestKey = null;
 
   const fetchData = async () => {
     try {
       if (isGetOne) {
         const record = await pb
           .collection(collectionId)
-          .getOne(recordId, { requestKey: null });
+          .getOne(recordId, options);
         setData(record);
       } else {
-        const records = await pb
-          .collection(collectionId)
-          .getFullList({ requestKey: null });
+        const records = await pb.collection(collectionId).getFullList(options);
         setData(records);
       }
       setReady(true);
